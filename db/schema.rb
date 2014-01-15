d697a99da534e228e117bcf43fe9efb61404942b
David <david.kenny2@mail.dcu.ie>
2014-01-15 23:37:29 +0000

    routine commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111221559) do
+ActiveRecord::Schema.define(version: 20140115213314) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
