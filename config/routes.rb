5dfc33d7eb3b48e998413063df3bed332c384168
David <david.kenny2@mail.dcu.ie>
2014-01-16 02:29:25 +0000

    pins and user associations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
+  get "notice" => "pins#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
