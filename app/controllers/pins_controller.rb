aeb3927bd9ac00e56aec97935fb750bbe9fb209f
David <david.kenny2@mail.dcu.ie>
2014-01-16 00:05:42 +0000

    cleaned up pins scaffold

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -26,39 +26,39 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
+        redirect_to @pin, notice: 'Pin was successfully created.' 
+        # format.json { render action: 'show', status: :created, location: @pin }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+         render action: 'new' 
+        # format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
-    end
+    # end
   end
 
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
-    respond_to do |format|
+   # respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @pin, notice: 'Pin was successfully updated.' 
+        # format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        ender action: 'edit' 
+        # format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
-    end
+   # end
   end
 
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url }
-      format.json { head :no_content }
-    end
+    # respond_to do |format|
+      redirect_to pins_url 
+      # format.json { head :no_content }
+    # end
   end
 
   private
